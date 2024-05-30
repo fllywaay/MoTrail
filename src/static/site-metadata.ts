1078c766d41b1efe43db4e5d51e5a7e33ea7f1c4
fllywaay <87158446+fllywaay@users.noreply.github.com>
2024-05-30 18:47:33 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,19 +11,15 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Workouts Map',
-  siteUrl: 'https://ben29.xyz',
-  logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
-  description: 'Personal site and blog',
+  siteTitle: '运动地图',
+  siteUrl: '',
+  logo: 'https://avatars.githubusercontent.com/u/87158446?v=4',
+  description: 'Flyway的运动地图',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://ben29.xyz',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/ben-29/workouts_page/blob/master/README-CN.md',
+      name: '🏎️🚲🏃🏽‍♂️🚶🏽‍♂️🏕️',
+      url: '',
     },
   ],
 };
